41acd266a820c3237ebd7e9ca58592f3b9a0aacc
miroslavamarcuk <m.marchuk@kitsoft.kiev.ua>
2022-03-08 12:17:08 +0100

    init

---
+++ build/precache-manifest.feb787192a3e1d1524a417a706452a38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2157b7d4d33b056fd1d618e803fcd26",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7e8375bfb211d5823bf7",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "91e61521b1f08e177bba",
+    "url": "./static/css/main.99c11140.chunk.css"
+  },
+  {
+    "revision": "7e8375bfb211d5823bf7",
+    "url": "./static/js/2.fd035709.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.fd035709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91e61521b1f08e177bba",
+    "url": "./static/js/main.b033b85e.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
